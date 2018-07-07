8229247c181eb5d46a9c93e415018d63da58803d
liyang <869214368@qq.com>
2018-07-07 22:31:09 +0800

    omit

--- ITA/FibonacciHeaps/fibonacciHeap.h
+++ ITA/FibonacciHeaps/fibonacciHeap.h
@@ -13,7 +13,8 @@ struct Node{
     Node* prev;
     Node* parent;
     Node* child;
-    Node(int key):key(key), degree(0), next(this), prev(this), parent(NULL), child(NULL){};
+    bool marked;
+    Node(int key):key(key), degree(0), next(this), prev(this), parent(NULL), child(NULL), marked(false){};
 };
 
 class FibonacciHeaps{
@@ -22,7 +23,7 @@ public:
     void insertKey(int key);
     int extractMin();
     void mergeHeaps(FibonacciHeaps* someHeap);
-    void decreaseKey();
+    void decreaseKey(Node* node, int key);
     void deleteKey();
     void printHeaps();
     bool empty();
@@ -33,10 +34,76 @@ private:
     Node* min;
     void deleteFibonacciHeaps();
     void consolidate(Node* curNode);
+    void cascadingMoveToRootList(Node* node);
 };
 
-void FibonacciHeaps::deleteFibonacciHeaps(){
+void removeNodeFromList(Node* node){
+    node->next->prev = node->prev;
+    node->prev->next = node->next;
+    node->next = node;
+    node->prev = node;
+}
 
+//node1, node2 belong to list1 and list2 separately
+void concatLists(Node* node1, Node* node2){
+    auto node1_next = node1->next;
+    auto node2_prev = node2->prev;
+    node1->next = node2;
+    node2->prev = node1;
+    node1_next->prev = node2_prev;
+    node2_prev->next = node1_next;
+}
+
+void FibonacciHeaps::cascadingMoveToRootList(Node* node){
+    if(node->parent == NULL){
+        return;
+    }
+    auto parent = node->parent;
+    if(node->next == node){
+        parent->child = NULL;
+    }else{
+        parent->child = node->next;
+        removeNodeFromList(node);
+    }
+    -- parent->degree;
+    node->parent = NULL;
+    node->marked = false;
+    concatLists(node, min);
+    if(node->key < min->key){
+        min = node;
+    }
+    if(parent->marked == false){
+        parent->marked = true;
+    }else{
+        cascadingMoveToRootList(parent);
+    }
+    
+}
+
+void FibonacciHeaps::decreaseKey(Node* node, int key){
+    if(node->key <= key)
+        return;
+    node->key = key;
+    cascadingMoveToRootList(node);
+}
+
+void FibonacciHeaps::deleteFibonacciHeaps(){
+    if(min == NULL) return;
+    queue<Node*> nodes;
+    nodes.push(min);
+    while(!nodes.empty()){
+        auto node = nodes.front();
+        auto saveNode = node;
+        do{
+            if(node->child != NULL){
+                nodes.push(node->child);
+            }
+            auto nextNode = node->next;
+            delete node;
+            node = nextNode;
+        }while(node != saveNode);
+        nodes.pop();
+    }
 }
 
 FibonacciHeaps::~FibonacciHeaps(){
@@ -63,28 +130,11 @@ void FibonacciHeaps::insertKey(int key){
     ++ num;
 }
 
-//node1, node2 belong to list1 and list2 separately
-void concatLists(Node* node1, Node* node2){
-    auto node1_next = node1->next;
-    auto node2_prev = node2->prev;
-    node1->next = node2;
-    node2->prev = node1;
-    node1_next->prev = node2_prev;
-    node2_prev->next = node1_next;
-}
-
-void removeNodeFromList(Node* node){
-    node->next->prev = node->prev;
-    node->prev->next = node->next;
-}
-
 void concatHeaps(Node* node1, Node* node2){
     removeNodeFromList(node2);
-
     //add node2 to node1's children list
-    node2->next = node2;
-    node2->prev = node2;
     node2->parent = node1;
+    ++ node1->degree;
     if(node1->child != NULL){
         concatLists(node2, node1->child);
         if(node2->key < node1->child->key){
@@ -93,7 +143,6 @@ void concatHeaps(Node* node1, Node* node2){
     }else{
         node1->child = node2;
     }
-    node1->degree += 1;
 }
 
 
@@ -101,35 +150,49 @@ int FibonacciHeaps::extractMin(){
     if(min == NULL) return -1;
     int minKey = min->key;
     if(min->child != NULL){
+        auto node = min->child;
+        do{
+            node->parent = NULL;
+            node = node->next;
+        }while(node != min->child);
         concatLists(min, min->child);
     }
 
     Node* curNode = min->next;
-    bool need_to_consolidate = curNode != min;
+    bool need_to_consolidate = (curNode != min);
 
     removeNodeFromList(min);
     delete min;
     min = NULL;
+    -- num;
 
     if(need_to_consolidate){
         consolidate(curNode);
+        /*
+        auto saveNode = curNode;
+        min = curNode;
+        do{
+            if(curNode->key < min->key){
+                min = curNode;
+            }
+            curNode = curNode->next;
+        }while(curNode != saveNode);
+        */
     }
-    -- num;
     return minKey;
 }
 
 void FibonacciHeaps::consolidate(Node* curNode){
     //traverse the root list to merge subtrees
-    vector<Node*> nodes(num, NULL); 
+    vector<Node*> nodes(100, NULL); 
     while(true){
         if(nodes[curNode->degree] == curNode){
             break;
         }
+        auto nextNode = curNode->next;
         if(nodes[curNode->degree] == NULL){
             nodes[curNode->degree] = curNode;
-            curNode = curNode->next;
         }else{
-            auto nextNode = curNode->next;
             while(nodes[curNode->degree] != NULL){
                 auto conflictNode = nodes[curNode->degree];
                 nodes[curNode->degree] = NULL;
@@ -141,12 +204,12 @@ void FibonacciHeaps::consolidate(Node* curNode){
                 }
             }
             nodes[curNode->degree] = curNode;
-            curNode = nextNode;
         }
+        curNode = nextNode;
     }
 
     //traverse the root list to find the min node
-    int newMinKey = __INT_MAX__;
+    int newMinKey = 100000;//__INT_MAX__;
     Node* newMinNode = NULL;
     for(Node* node : nodes){
         if(node != NULL && node->key < newMinKey){
--- ITA/FibonacciHeaps/main.cpp
+++ ITA/FibonacciHeaps/main.cpp
@@ -5,13 +5,17 @@
 int main(){
     FibonacciHeaps heaps;
     srand(time(NULL));
-    for(int i = 0; i != 10; ++i){
+    for(int i = 0; i != 90; ++i){
         heaps.insertKey(rand()%1000);
     }
     heaps.printHeaps();
+    int count = 0;
     for(; !heaps.empty();){
-        cout<<heaps.extractMin()<<endl;
-        heaps.printHeaps();
+        int key = heaps.extractMin();
+        cout<<(count++)<<" "<<key<<endl;
+//        heaps.printHeaps();
+        if(key == -1)
+            break;
     }
     return 0;
 }
